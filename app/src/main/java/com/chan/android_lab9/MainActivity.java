ddb9299b7d7c7da5820e6dfc0a27003fd576c6f2
Simon Chan <619156737@qq.com>
2017-12-15 08:00:51 +0800

    null string detail.java 60

--- app/src/main/java/com/chan/android_lab9/MainActivity.java
+++ app/src/main/java/com/chan/android_lab9/MainActivity.java
@@ -1,5 +1,6 @@
 package com.chan.android_lab9;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,29 +8,29 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.chan.android_lab9.Adapter.CommonAdapter;
 import com.chan.android_lab9.Adapter.ViewHolder;
 import com.chan.android_lab9.model.Github;
 import com.chan.android_lab9.service.GithubService;
 
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-import io.reactivex.Observer;
-import io.reactivex.Scheduler;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
-import io.reactivex.subjects.Subject;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Scheduler;
 import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,8 +38,10 @@ public class MainActivity extends AppCompatActivity {
     Button clearBtn;
     Button fetchBtn;
     RecyclerView contentList;
+    ProgressBar waitPrograss;
     List<Map<String, Object>> GithubList = new ArrayList<>();
     CommonAdapter GithubAdapter;
+    private GithubService githubservice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,8 +51,10 @@ public class MainActivity extends AppCompatActivity {
         clearBtn = findViewById(R.id.clearBtn);
         fetchBtn = findViewById(R.id.fetchBtn);
         contentList = findViewById(R.id.recycler_view);
-        Retrofit GithubRetrofit = createRetrofit("https://api.github.com/");
-        final GithubService githubservice = GithubRetrofit.create(GithubService.class);
+        waitPrograss = findViewById(R.id.mian_prograss);
+
+        Retrofit GithubRetrofit = retrofitFactory.createRetrofit("https://api.github.com/");
+        githubservice = GithubRetrofit.create(GithubService.class);
 
         contentList.setLayoutManager(new LinearLayoutManager(this));
         GithubAdapter = new CommonAdapter(this, R.layout.main_item_layout, GithubList) {
@@ -63,55 +68,54 @@ public class MainActivity extends AppCompatActivity {
                 blog.setText(s.get("blog").toString());
             }
         };
+        contentList.setAdapter(GithubAdapter);
+        GithubAdapter.setOnItemClickListener(new CommonAdapter.OnItemClickListener() {
+            @Override
+            public void onClick(int position) {
+                Intent intent = new Intent(MainActivity.this, detail.class);
+                intent.putExtra("name", GithubAdapter.getName(position));
+                startActivity(intent);
+            }
+
+            @Override
+            public boolean onLongClick(int position) {
+                GithubAdapter.removeData(position);
+                return true;
+            }
+        });
 
         clearBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                searchEdit.setText("");
+                GithubAdapter.clearData();
             }
         });
         fetchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String User = searchEdit.getText().toString();
+                waitPrograss.setVisibility(View.VISIBLE);
                 githubservice.getUser(User)
                         .subscribeOn(Schedulers.newThread())
                         .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(new Subscriber<Github>(){
+                        .subscribe(new Subscriber<Github>() {
                             @Override
                             public void onCompleted() {
-                                removeWait();
+                                waitPrograss.setVisibility(View.GONE);
                             }
 
                             @Override
                             public void onError(Throwable e) {
-
+                                Toast.makeText(MainActivity.this, e.getMessage()+"确认你搜索的用户存在", Toast.LENGTH_LONG).show();
+                                waitPrograss.setVisibility(View.GONE);
                             }
 
                             @Override
                             public void onNext(Github github) {
-
+                                GithubAdapter.addData(github);
                             }
                         });
             }
         });
     }//end onCreate
-
-    private static Retrofit createRetrofit(String baseUrl){
-        return new Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .client(createOkHttp())
-                .build();
-    }
-
-    private static OkHttpClient createOkHttp(){
-        OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                .connectTimeout(10, TimeUnit.SECONDS)
-                .readTimeout(30, TimeUnit.SECONDS)
-                .writeTimeout(10, TimeUnit.SECONDS)
-                .build();
-        return okHttpClient;
-    }
 }
